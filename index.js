d4e2344ac8e52ddb8369a065d1b2b4ab45618946
AaronJHarvey <61170939+AaronJHarvey@users.noreply.github.com>
2020-11-09 20:03:17 -0500

    dry code

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 const endPoint = 'http://localhost:3000/api/v1/gifts'
 
+
+
+
 document.addEventListener('DOMContentLoaded',() => {
   getGifts()
-
   const createGiftForm = document.querySelector('#create-gift-form')
 
   createGiftForm.addEventListener('submit', (e)=>createFormHandler(e))
 })
+
+
+
+
 
 function getGifts(){
   fetch(endPoint)
   .then(response =>response.json())
   .then(giftItems => {
     giftItems.data.forEach(gift =>{
-      const giftMarkup = `
-      <div data-id=${gift.id}>
-        <h3> ${gift.attributes.name}</h3>
-        <p>${gift.attributes.store}</p>
-        <p>for ${gift.attributes.person.name}</p>
-        <button data-id=${gift.id}>edit</button>
-      </div>
-      <br><br>`;
-
-      document.querySelector('#gift-container').innerHTML += giftMarkup
+      render(gift)
     })
   })
 }
+
+
+
+
+  function render(gift){
+    const giftMarkup = `
+    <div data-id=${gift.id}>
+      <h3> ${gift.attributes.name}</h3>
+      <p>${gift.attributes.store}</p>
+      <p>for ${gift.attributes.person.name}</p>
+      <button data-id=${gift.id}>edit</button>
+    </div>
+    <br><br>`;
+
+    document.querySelector('#gift-container').innerHTML += giftMarkup;
+  }
+
+
+
+
+
 
 function createFormHandler(e){
   e.preventDefault()
@@ -35,6 +53,12 @@ function createFormHandler(e){
   postGift(giftNameInput, giftStoreNameInput, personId)
 }
 
+
+
+
+
+
+
 function postGift(name, store, person_id){
   let bodyData = {name,store,person_id}
   fetch(endPoint,{
@@ -43,7 +67,10 @@ function postGift(name, store, person_id){
     body: JSON.stringify(bodyData)
   })
   .then(resp => resp.json())
-  .then(data=>{
-    console.log(data)
+  .then(gift=>{
+    console.log(data);
+    const giftData = gift.data
+    render(gift)
   })
+  .catch(err=>console.log(err))
 }
